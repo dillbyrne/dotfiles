b71eed1afc513d490442466aa8ed3cf1d3fe6ec3
dillbyrne <dillbyrne@users.noreply.github.com>
2017-07-06 20:51:36 +0100

    Check for binaries, updated plugins to latest versions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+check_binaries(){
+
+BINS=(flake8 jshint)
+
+for i in ${BINS[*]}
+do
+	type $i >/dev/null 2>&1 ||\
+		{ echo >&2 "$i is not installed."; }
+done
+
+}
+
+
 create_symlinks(){
+
 	# vim
 	ln -s ~/dotfiles/vim ~/.vim
 	ln -s ~/dotfiles/vim/vimrc ~/.vimrc
@@ -13,9 +27,10 @@ create_symlinks(){
 }
 
 
-
 main(){
 	create_symlinks
+	check_binaries
 }
+
 
 main
