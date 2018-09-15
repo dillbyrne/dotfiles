85a258873942f5a5c724eff9d1a50893083e57b0
dillbyrne <dillbyrne@users.noreply.github.com>
2018-09-15 17:40:45 +0100

    Added readme changed vimrc to default to python3 linting

---
+++ README.md
@@ -0,0 +1,13 @@
+# Setup instructions
+
+
+https://gist.github.com/dillbyrne/e484a633117bffb4e0616c4b71a79d25
+
+run the following in your home dir
+git clone --recursive https://github.com/dillbyrne/dotfiles.git
+
+then run
+
+setup.sh
+
+
--- setup.sh
+++ setup.sh
@@ -20,10 +20,10 @@ create_symlinks(){
 	ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 
 	# git
-	ln -s ~/dotfiles/gitconfig ~/.gitconfig
+	#ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 	# npm
-	ln -s ~/dotfiles/npmrc ~/.npmrc
+	#ln -s ~/dotfiles/npmrc ~/.npmrc
 }
 
 
