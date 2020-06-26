c8ad6a45e9cf7affa85ea8fbe8079c3f66d9f664
dillbyrne <dillbyrne@users.noreply.github.com>
2020-06-26 21:09:39 +0100

    install jshint for jsdev

--- setup.sh
+++ setup.sh
@@ -15,39 +15,53 @@ check_binaries(){
 }
 
 
+
 setup_fonts(){
 	mkdir nerd_fonts
-	cd nerd_fonts
+	cd nerd_fonts || exit 1
 	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/LiberationMono.zip
 	unzip LiberationMono.zip
 	rm *Windows* LiberationMono.zip
 	cp * ~/.local/share/fonts
-	cd -
+	cd - || exit 1
 
+	# copy the fonts to ./local/share/fonts 
+	# and refresh the font cache
 	./fonts/install.sh
+
+	# delete nerd_fonts dir if you want
+	# personally I keep it 
+	# rm -rf nerd_fonts
+
+}
+
+setup_packages(){
+	# add ./npm-packages/bin to .bashrc path
+	# export PATH=$PATH:$HOME/npm-packages/bin
+
+	npm link jshint
 
 }
 
 create_symlinks(){
-
-	#TODO check if symlinks exist
-
+	
 	# vim
 	ln -s ~/dotfiles/vim ~/.vim
 	ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 
 	# git
-	#ln -s ~/dotfiles/gitconfig ~/.gitconfig
+	ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 	# npm
-	#ln -s ~/dotfiles/npmrc ~/.npmrc
+	ln -s ~/dotfiles/npmrc ~/.npmrc
 }
 
 
 main(){
 #	create_symlinks
-	setup_fonts
-#	check_binaries
+#	setup_fonts
+	check_binaries
+#	setup_packages
 }
 
 
